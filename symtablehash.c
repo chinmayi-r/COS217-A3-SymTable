0813b4e16a807adb93210ece1d6255097efd6aa1
Chinmayi Ramasubramanian <cr7998@princeton.edu>
2025-03-25 13:28:37 -0400

    Edited code

--- symtablehash.c
+++ symtablehash.c
@@ -1,6 +1,7 @@
 #include "symtable.h"
 
-enum { BUCKET_COUNT = 509 };
+static size_t BUCKET_COUNT[] = {509, 1021, 2039, 4093, 8191, 16381, 32749, 65521};
+/*enum { BUCKET_COUNT = 509 };*/
 
 typedef struct Binding Binding_T;
 struct Binding {
@@ -42,10 +43,10 @@ SymTable_T SymTable_new()
  struct Binding **q;
  p = (struct SymTable *) calloc(1, sizeof(*p));
  if(p == NULL) {printf("Mem alloc Error"); return NULL;}
- q = (struct Binding **) calloc(BUCKET_COUNT, sizeof(*q));
+ q = (struct Binding **) calloc(BUCKET_COUNT[0], sizeof(*q));
  if(q == NULL) {printf("Mem alloc Error"); return NULL;}
  p->buckets = q;
- p->size = BUCKET_COUNT;
+ p->size = BUCKET_COUNT[0];
  p->len = 0;
  return p;
 }
@@ -75,9 +76,50 @@ void SymTable_free(SymTable_T oSymTable)
 
 size_t SymTable_getLength(SymTable_T oSymTable){assert(oSymTable != NULL); return oSymTable->len;}
 
+static void SymTable_resize(SymTable_T oSymTable, size_t size)
+{
+    Binding_T **old_buckets;
+    Binding_T **new_buckets;
+    Binding_T *buckets_i;
+    Binding_T *next; 
+    size_t i;
+
+    size_t old_size = oSymTable->size;
+    old_buckets = oSymTable->buckets;
+    new_buckets = calloc(size, sizeof(*new_buckets));
+    if(new_buckets == NULL) {printf("Mem alloc Error"); return;}
+
+    oSymTable->buckets = new_buckets;
+    oSymTable->size = size;
+    oSymTable->len = 0; /* Since SymTable_put increments len when reinserting bindings*/
+
+	for (i = 0; i < old_size; i++) {
+        buckets_i = old_buckets[i];
+        while (buckets_i != NULL)
+        {
+            next = buckets_i->next;
+            SymTable_put(oSymTable, buckets_i->key, buckets_i->value);
+            Binding_free(buckets_i);
+            buckets_i = next;
+        }
+	}
+	free(old_buckets);
+    return;
+}
+
+int findIndex(size_t arr[], size_t size, size_t value) {
+    size_t i;
+    for (i = 0; i < size; i++) {
+        if (arr[i] == value) {
+            return i;
+        }
+    }
+    return -1;
+}
+
 int SymTable_put(SymTable_T oSymTable, const char *pcKey, const void *pvValue)
 {
-    size_t hash_value = SymTable_hash(pcKey, oSymTable->size);
+    size_t hash_value;
     Binding_T *newBinding;
 
     assert(oSymTable != NULL);
@@ -85,7 +127,15 @@ int SymTable_put(SymTable_T oSymTable, const char *pcKey, const void *pvValue)
     /*assert(pvValue != NULL);*/
 
     if(SymTable_contains(oSymTable, pcKey)){return 0;}
+
+    /* NOTE that >= does not mean no of elements >= BUCKETCOUNT!!! 
+    Since ++(oSymTable->len); is happening later, the no of elements is actually (oSymTable->len+1) !*/
+    if ((oSymTable->len >= oSymTable->size) && oSymTable->size != BUCKET_COUNT[7])
+    {
+        SymTable_resize(oSymTable, BUCKET_COUNT[findIndex(BUCKET_COUNT, 8, oSymTable->size)+1]);
+    }
     
+    hash_value = SymTable_hash(pcKey, oSymTable->size);
     newBinding = (Binding_T *) calloc(1, sizeof(Binding_T));
     if(newBinding == NULL) {printf("Mem alloc Error"); return 0;}
     newBinding->key = (const char*)malloc(strlen(pcKey) + 1);
