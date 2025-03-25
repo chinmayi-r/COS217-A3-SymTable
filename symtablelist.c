77e98ee1a8532c190fdc1923d2b2bb583d0d7827
Chinmayi Ramasubramanian <cr7998@princeton.edu>
2025-03-25 14:30:12 -0400

    Edited comments

--- symtablelist.c
+++ symtablelist.c
@@ -7,8 +7,8 @@
 /* A Node_T object represents a single key-value binding within a symbol table.
    Each node contains a unique string key, an associated value pointer, 
    and a pointer to the next binding in a linked list. */
-typedef struct Node Node_T;
 struct Node {const char *key; const void *value; struct Node *next;};
+typedef struct Node Node_T;
 
 /* What is the significance of typedef struct SymTable *SymTable_T; instead of typedef struct SymTable SymTable_T;?*/
 
@@ -16,10 +16,10 @@ struct SymTable {struct Node *first; size_t len;};
 
 /* Free the memory associated with the Node structure p, including
    its key but not the value it points to. */
-static void Node_free(Node_T *p){
-    assert(p != NULL);
-    free((char *)p->key);
-    free(p);
+static void Node_free(Node_T *temp_p){
+    assert(temp_p != NULL);
+    free((char *)temp_p->key);
+    free(temp_p);
 }
 
 /* Why are we making a list with just pointer first, cant we directly refer to list using a pointer: SymTable *root = ...*/
@@ -29,25 +29,25 @@ static void Node_free(Node_T *p){
    as its bucket count. */
 SymTable_T SymTable_new()
 {
- struct SymTable *p; /* So can I replace this with SymTable_T p;? or is it SymTable_T *p;? */
- p = calloc(1, sizeof(*p));
- if(p == NULL) { printf("Mem alloc Error"); return NULL;}
- p->len = 0;
- return p;
+ struct SymTable *temp_p; /* So can I replace this with SymTable_T p;? or is it SymTable_T *p;? */
+ temp_p = calloc(1, sizeof(*temp_p));
+ if(temp_p == NULL) { printf("Mem alloc Error"); return NULL;}
+ temp_p->len = 0;
+ return temp_p;
 }
 
 /* Free all memory associated with the symbol table oSymTable, 
    including all bindings and the table structure itself. */
 void SymTable_free(SymTable_T oSymTable)
 {
-    Node_T *p = oSymTable->first;
+    Node_T *temp_p = oSymTable->first;
     Node_T *next;
 
     assert(oSymTable != NULL);
 
-    for ( ; p != NULL; p = next) {
-        next = p->next;
-        Node_free(p);
+    for ( ; temp_p != NULL; temp_p = next) {
+        next = temp_p->next;
+        Node_free(temp_p);
     }
     /* free(oSymTable->first);  Is this needed? Is double freeing happening?*/
     free(oSymTable);
@@ -84,20 +84,20 @@ int SymTable_put(SymTable_T oSymTable, const char *pcKey, const void *pvValue)
    Returns the old value associated with pcKey if it exists, otherwise returns NULL. */
 void *SymTable_replace(SymTable_T oSymTable, const char *pcKey, const void *pvValue)
 {
-    Node_T *p = oSymTable->first;
+    Node_T *temp_p = oSymTable->first;
     const void *temp;
     
     assert(oSymTable != NULL);
     assert(pcKey != NULL);
     /*assert(pvValue != NULL);*/
 
-    if (p == NULL) {return NULL;}
+    if (temp_p == NULL) {return NULL;}
 
-    for (; p != NULL; p = p->next)
+    for (; temp_p != NULL; temp_p = temp_p->next)
     {
-        if (strcmp(p->key, pcKey) == 0) {
-            temp = p->value;
-            p->value = pvValue;
+        if (strcmp(temp_p->key, pcKey) == 0) {
+            temp = temp_p->value;
+            temp_p->value = pvValue;
             return (void *) temp;}
     }
     return NULL;
@@ -107,16 +107,16 @@ void *SymTable_replace(SymTable_T oSymTable, const char *pcKey, const void *pvVa
    Returns 1 if pcKey is found, 0 otherwise. */
 int SymTable_contains(SymTable_T oSymTable, const char *pcKey)
 {
-    Node_T *p = oSymTable->first;
+    Node_T *temp_p = oSymTable->first;
     
     assert(oSymTable != NULL);
     assert(pcKey != NULL);
 
-    if (p == NULL) {return 0;}
+    if (temp_p == NULL) {return 0;}
 
-    for (; p != NULL; p = p->next) 
+    for (; temp_p != NULL; temp_p = temp_p->next) 
     {
-        if (strcmp(p->key, pcKey) == 0) 
+        if (strcmp(temp_p->key, pcKey) == 0) 
             return 1;
     }
     return 0;
@@ -126,17 +126,17 @@ int SymTable_contains(SymTable_T oSymTable, const char *pcKey)
    Returns NULL if pcKey is not found. */
 void *SymTable_get(SymTable_T oSymTable, const char *pcKey)
 {
-    Node_T *p = oSymTable->first;
+    Node_T *temp_p = oSymTable->first;
 
     assert(oSymTable != NULL);
     assert(pcKey != NULL);
 
-    if (p == NULL) {return NULL;}
+    if (temp_p == NULL) {return NULL;}
 
-    for (; p != NULL; p = p->next) 
+    for (; temp_p != NULL; temp_p = temp_p->next) 
     {
-        if (strcmp(p->key, pcKey) == 0) 
-            return (void *) p->value;
+        if (strcmp(temp_p->key, pcKey) == 0) 
+            return (void *) temp_p->value;
     }
     return NULL;
 }
@@ -145,26 +145,26 @@ void *SymTable_get(SymTable_T oSymTable, const char *pcKey)
    Returns the value associated with pcKey, or NULL if pcKey is not found. */
 void *SymTable_remove(SymTable_T oSymTable, const char *pcKey)
 {
-    Node_T *p = oSymTable->first;
+    Node_T *temp_p = oSymTable->first;
     Node_T *prev;
     const void *temp;
     
     assert(oSymTable != NULL);
     assert(pcKey != NULL);
 
-    if (p == NULL) {return NULL;}
+    if (temp_p == NULL) {return NULL;}
 
     prev = NULL;
-    for ( ; p != NULL; p = p->next) {
-        if (strcmp(p->key, pcKey) == 0) { /* Diff from above!!!!! Note differences!!!*/
-            if (prev == NULL) {oSymTable->first = p->next;}
-            else {prev->next = p->next;}
+    for ( ; temp_p != NULL; temp_p = temp_p->next) {
+        if (strcmp(temp_p->key, pcKey) == 0) { /* Diff from above!!!!! Note differences!!!*/
+            if (prev == NULL) {oSymTable->first = temp_p->next;}
+            else {prev->next = temp_p->next;}
             --(oSymTable->len);
-            temp = p->value;
-            Node_free(p);
+            temp = temp_p->value;
+            Node_free(temp_p);
             return (void *) temp;
         }
-        prev = p;
+        prev = temp_p;
     }
     return NULL;
 }
@@ -173,13 +173,13 @@ void *SymTable_remove(SymTable_T oSymTable, const char *pcKey)
    passing pcKey, pvValue, and pvExtra as arguments. */
 void SymTable_map(SymTable_T oSymTable, void (*pfApply)(const char *pcKey, void *pvValue, void *pvExtra), const void *pvExtra)
 {
-    Node_T *p = oSymTable->first;
+    Node_T *temp_p = oSymTable->first;
     
     assert(oSymTable != NULL);
     /*assert(pvExtra != NULL); Can It be null?? Is assert needed* - NO. See Ed https://edstem.org/us/courses/74019/discussion/6328027/ */
 
     /*assert(pfApply != NULL);Can It be null?? Is assert needed*/
 
-    for ( ; p != NULL; p = p->next)
-    (*pfApply)(p->key, (void *) p->value, (void *) pvExtra); /* Am I supposed to cast here with (void *) like this?*/
+    for ( ; temp_p != NULL; temp_p = temp_p->next)
+    (*pfApply)(temp_p->key, (void *) temp_p->value, (void *) pvExtra); /* Am I supposed to cast here with (void *) like this?*/
 }
